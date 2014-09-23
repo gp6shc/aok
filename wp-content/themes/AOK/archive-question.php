1738e902c57bc9cae438a2e86a3b08d393449251
Mike <mike@sachsmedia.com>
2014-09-23 18:46:20 -0400

    block-level links to full posts

--- wp-content/themes/AOK/archive-question.php
+++ wp-content/themes/AOK/archive-question.php
@@ -15,32 +15,35 @@
 
 <?php while ( have_posts() ) : the_post() ?>
 <h2 class="date"><?php the_date('','<p>','</p>'); ?></h2>   
-<div <?php post_class( 'question'); ?> id="post-<?php the_ID(); ?>">
 
-<!-- <div class="questionnumber"><p><a href="<?php the_permalink(); ?>#comments"><?php comments_number( '' . __('0', 'wp-answers') . '', '' . __('1', 'wp-answers') . '', '' . __('%', 'wp-answers') . '' ); ?></a></p></div> -->
+<a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" <?php post_class( 'question'); ?> id="post-<?php the_ID(); ?>">
 
-<div class="avatar"><?php echo get_user_avatar(get_the_author_meta('ID'),50) ?></div>
-<div class="questionmain">
+	<div class="avatar"><?php echo get_user_avatar(get_the_author_meta('ID'),50) ?></div>
+	
+	<div class="questionmain">
+		<h2 class="questiontitle">
+			<?php if (strlen($post->post_title) > 45) {
+			    	echo substr(the_title($before = '', $after = '', FALSE), 0, 45) . '.. <i class="fa fa-caret-right"></i>'; 
+			    } else {
+			    	the_title();
+			    } ?>
+		</h2>
+		
+		<ul class="questionmeta">
+			<li>by: <?php the_author_meta( 'first_name' ); ?> <?php the_author_meta( 'last_name' ); ?>
+			
+				<span>
+				    <?=function_exists('thumbs_rating_show_up_votes') ? thumbs_rating_show_down_votes() : ''?>
+				    <i class="fa fa-thumbs-down"> | </i>
+				    <?=function_exists('thumbs_rating_show_up_votes') ? thumbs_rating_show_up_votes() : ''?>
+				    <i class="fa fa-thumbs-up"></i>
+				</span>
+			</li>
+		</ul>
+		
+	</div>
 
-<h2 class="questiontitle">
-<a href="<?php the_permalink() ?>" rel="bookmark" title="Read <?php the_title_attribute(); ?>">
-<?php if (strlen($post->post_title) > 45) {
-echo substr(the_title($before = '', $after = '', FALSE), 0, 45) . '.. <i class="fa fa-caret-right"></i>'; } else {
-the_title();
-} ?>
-</a>
-</h2>
-
-<ul class="questionmeta">
-
-<li>Submitted on on <?php the_time('F j, Y') ?> | <?php _e('by:','wp-answers') ?> <a class="url fn n" href="<?php echo get_the_author_meta('user_url'); ?>" target="_blank"><?php the_author_meta( 'first_name' ); ?> <?php the_author_meta( 'last_name' ); ?></a></li>
-
-
-</div>
-
-</ul>
-
-</div><!-- End Question-->
+</a><!-- End Question-->
 
 <?php endwhile; ?>
 <?php //wp_reset_query(); ?> 
